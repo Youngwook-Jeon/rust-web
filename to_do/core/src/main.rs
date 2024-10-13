9c7cffb395078c1f78864592a22c8360a112c5e2
Youngwook-Jeon <ela87@naver.com>
2024-10-13 20:16:36 +0900

    First commit

---
+++ to_do/core/src/main.rs
@@ -0,0 +1,15 @@
+mod api;
+mod enums;
+mod structs;
+
+use structs::done::Done;
+use structs::pending::Pending;
+
+fn main() {
+    let done = Done::new("shopping");
+    println!("{}", done.super_struct.title);
+    println!("{}", done.super_struct.status);
+    let pending = Pending::new("laundry");
+    println!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.status);
+}
