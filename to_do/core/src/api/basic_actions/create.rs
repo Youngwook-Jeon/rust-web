36484c0c15bcc699e812979007deeae56eb4d5de
Youngwook-Jeon <ela87@naver.com>
2024-11-10 19:35:17 +0900

    Refactoring our core api actions

--- to_do/core/src/api/basic_actions/create.rs
+++ to_do/core/src/api/basic_actions/create.rs
@@ -1,27 +1,14 @@
 use dal::json_file::save_one;
 
 use crate::enums::TaskStatus;
-use crate::structs::{done::Done, pending::Pending};
-use std::fmt;
+use crate::structs::ToDoItem;
 
-pub enum ItemTypes {
-    Pending(Pending),
-    Done(Done),
-}
+pub fn create(title: &str, status: TaskStatus) -> Result<ToDoItem, String> {
+    let item = ToDoItem {
+        title: title.to_string(),
+        status,
+    };
 
-impl fmt::Display for ItemTypes {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ItemTypes::Pending(pending) => write!(f, "Pending: {}", pending.super_struct.title),
-            ItemTypes::Done(done) => write!(f, "Done: {}", done.super_struct.title),
-        }
-    }
-}
-
-pub fn create(title: &str, status: TaskStatus) -> Result<ItemTypes, String> {
-    let _ = save_one(&title.to_string(), &status)?;
-    match &status {
-        TaskStatus::PENDING => Ok(ItemTypes::Pending(Pending::new(&title))),
-        TaskStatus::DONE => Ok(ItemTypes::Done(Done::new(&title))),
-    }
+    let _ = save_one(&title.to_string(), &item)?;
+    Ok(item)
 }
--- to_do/networking/actix_server/src/api/basic_actions/mod.rs
+++ to_do/networking/actix_server/src/api/basic_actions/mod.rs
@@ -1,4 +1,10 @@
+use actix_web::web::{get, scope, ServiceConfig};
+
 pub mod create;
 pub mod delete;
 pub mod get;
 pub mod update;
+
+pub fn basic_actions_factory(app: &mut ServiceConfig) {
+    app.service(scope("/api/v1").route("get/all", get().to(get::get_all)));
+}
